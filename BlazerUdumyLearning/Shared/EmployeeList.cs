a468f7344753d742c7d61ab3eef4658116148821
RESHMY P P <reshmy.p.p@gmail.com>
2021-09-14 16:03:47 +0400

    data binding example

---
+++ BlazerUdumyLearning/Shared/EmployeeList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazerUdumyLearning.Shared
+{
+    public class EmployeeList
+    {
+        public string Name { get; set; }
+        public string Qualification { get; set; }
+    }
+}
